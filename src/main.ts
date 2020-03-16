29ebe6549e21ed2b626faa825bc517b50318cd7c
matthiasunt <17273057+matthiasunt@users.noreply.github.com>
2020-03-16 19:14:24 +0100

    Add security packages

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
 require('dotenv').config({ path: '.env' });
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 1000, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
 
   await app.listen(process.env.PORT);
 }
