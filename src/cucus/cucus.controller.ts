9a595966612334a03f0a2cb812393e168676e345
matthiasunt <17273057+matthiasunt@users.noreply.github.com>
2020-10-27 20:54:02 +0100

    Refactoring

--- src/cucus/cucus.controller.ts
+++ src/cucus/cucus.controller.ts
@@ -17,11 +17,6 @@ export class CucusController {
   ) {
   }
 
-  // @Get()
-  // async findAll(): Promise<Cucu[]> {
-  //   return this.cucusService.findAll();
-  // }
-
   @Get(':lang/after/:date')
   async findUpcomingByLanguage(@Param('lang') lang: string, @Param('date') date: string): Promise<Cucu[]> {
     return this.cucusService.findByLanguageAndDate(lang, date);
